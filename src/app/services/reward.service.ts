cc0ef9167df074a78a7d73a6e364742293ca743c
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-08-24 21:45:11 +0300

    fix: Weird japanese stage throwing errors in global

--- src/app/services/reward.service.ts
+++ src/app/services/reward.service.ts
@@ -1,56 +1,60 @@
-import { Injectable } from "@angular/core";
-import { DataService } from "./data.service";
-import { Reward } from "../entities/enum";
-import { CURRENCY_OFFSET, GACHA_END_OFFSET, GACHA_OFFSET } from "../entities/deck";
-import { Campaign } from "../entities/stage";
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Reward } from '../entities/enum';
+import { CURRENCY_OFFSET, GACHA_END_OFFSET, GACHA_OFFSET } from '../entities/deck';
+import { Campaign } from '../entities/stage';
 
 @Injectable({
-	providedIn: "root"
+	providedIn: 'root',
 })
 export class RewardService {
-	constructor(private dataService:DataService) {
-	}
+	constructor(private dataService: DataService) {}
 
 	convertGachaRewards(reward: Reward): Reward[] {
 		if (!(reward[0] >= GACHA_OFFSET && reward[1] < GACHA_END_OFFSET)) {
-			return []
+			return [];
 		}
-		const groupId = reward[0] - 4000000
+		const groupId = reward[0] - 4000000;
 		const group = this.dataService.common.GachaGroup.find((group) => group.Id === groupId);
 		if (!group && !((group.Id >= 600000 && group.Id < 700000) || (group.Id >= 300000 && group.Id < 360000))) {
-			return []
+			return [];
 		}
-		const totalProbability = group.ItemList.reduce((pv, cv) => {return pv + cv[1]}, 0) / reward[1]
-		const result: Reward[] = []
+		const totalProbability =
+			group.ItemList.reduce((pv, cv) => {
+				return pv + cv[1];
+			}, 0) / reward[1];
+		const result: Reward[] = [];
 		for (const item of group.ItemList) {
-			let [itemID, probability, ..._] = item
-			probability /= totalProbability
-			const equipment = this.dataService.equipments.get(itemID)
+			let [itemID, probability, ..._] = item;
+			probability /= totalProbability;
+			const equipment = this.dataService.equipments.get(itemID);
 			if (equipment) {
-				result.push([itemID, probability])
+				result.push([itemID, probability]);
 			}
 		}
-		return result
+		return result;
 	}
 
 	mergeRewards(base: Reward[], sub: Reward[]) {
-		let rewards: Reward[] = JSON.parse(JSON.stringify(base))
-		let ids = rewards.map(reward => reward[0])
+		let rewards: Reward[] = JSON.parse(JSON.stringify(base));
+		let ids = rewards.map((reward) => reward[0]);
 		for (const newReward of sub) {
 			if (ids.includes(newReward[0])) {
-				const index = rewards.findIndex(reward => reward[0] == newReward[0])
-				rewards[index][1] += newReward[1]
+				const index = rewards.findIndex((reward) => reward[0] == newReward[0]);
+				rewards[index][1] += newReward[1];
 			} else {
-				rewards.push(newReward)
+				rewards.push(newReward);
 			}
 		}
-		return rewards
+		return rewards;
 	}
 
 	createRewardForCampaign(campaign: Campaign) {
 		let baseRewards = campaign.regionalRewards(this.dataService)?.default;
 		let dropRewards = baseRewards?.filter((reward) => reward[0] < CURRENCY_OFFSET) ?? [];
-		const gachaRewards = baseRewards?.filter((reward) => reward[0] >= GACHA_OFFSET && reward[0] < GACHA_END_OFFSET).flatMap((reward) => this.convertGachaRewards(reward))
-		return this.mergeRewards(dropRewards, gachaRewards)
+		const gachaRewards = baseRewards
+			?.filter((reward) => reward[0] >= GACHA_OFFSET && reward[0] < GACHA_END_OFFSET)
+			.flatMap((reward) => this.convertGachaRewards(reward));
+		return this.mergeRewards(dropRewards, gachaRewards).filter((reward) => this.dataService.getStuff(reward[0]) !== undefined);
 	}
 }
